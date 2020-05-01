e4b7785e6fd503ad7c2fbfee1f331f74e40d450b
shrinathjoshi <shrinathjoshi1997@gmail.com>
2020-05-01 22:07:41 +0530

    Adding FirstBadVersion may week1 Prob 1

---
+++ May Challenge/week1/FirstBadVersion_LC-278.java
@@ -0,0 +1,20 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        //Binary Search
+        int low =1;
+        int high =n;
+        while(high>=low){
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+
+        return low;
+    }
+}
