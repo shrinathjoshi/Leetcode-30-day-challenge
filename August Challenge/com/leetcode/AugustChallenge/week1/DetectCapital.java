babca760dc779227f867e1a9798eb3277435574b
shrinathjoshi <shrinathjoshi1997@gmail.com>
2020-08-02 13:33:44 +0530

    adding question details

--- August Challenge/com/leetcode/AugustChallenge/week1/DetectCapital.java
+++ August Challenge/com/leetcode/AugustChallenge/week1/DetectCapital.java
@@ -1,7 +1,34 @@
 package com.leetcode.AugustChallenge.week1;
 
+/*
+https://leetcode.com/problems/detect-capital/
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ 
+
+Example 1:
+
+Input: "USA"
+Output: True
+ 
+
+Example 2:
+
+Input: "FlaG"
+Output: False
+
+
+ */
 public class DetectCapital {
 
+	// Time Complexity :- O(n)
+	// Space Complexity :- O(1)
 	public boolean detectCapitalUse(String word) {
 		int n = word.length();
 		if (n == 1)
