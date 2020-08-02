4c0c2da6061561c0dbca052f5630375fa95fad6a
shrinathjoshi <shrinathjoshi1997@gmail.com>
2020-08-02 13:29:31 +0530

    Leetcode August challenge day 1

---
+++ August Challenge/com/leetcode/AugustChallenge/week1/DetectCapital.java
@@ -0,0 +1,27 @@
+package com.leetcode.AugustChallenge.week1;
+
+public class DetectCapital {
+
+	public boolean detectCapitalUse(String word) {
+		int n = word.length();
+		if (n == 1)
+			return true;
+
+		if (Character.isUpperCase(word.charAt(0)) && Character.isUpperCase(word.charAt(1))) {
+			for (int i = 2; i < n; i++) {
+				if (Character.isLowerCase(word.charAt(i)))
+					return false;
+			}
+		} else {
+			for (int i = 1; i < n; i++) {
+				if (Character.isUpperCase(word.charAt(i)))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean detectCapitalUseUsingRegex(String word) {
+		return word.matches("[A-Z]*|.[a-z]*");
+	}
+}
